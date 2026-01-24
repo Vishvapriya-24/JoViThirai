53d37f59b0c067de64c2ad3fb058090de9a88e4e
Vishvapriya M <mvishvapriyamurugan@gmail.com>
2026-01-24 10:00:17 +0530

    Update Database.js

--- backend/Database.js
+++ backend/Database.js
@@ -1,14 +1,8 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    waitForConnections: true,
-  connectionLimit: 10,
-});
+export const db = mysql.createPool(process.env.DATABASE_URL);
+
 
 db.connect((err) => {
     if (err) {
