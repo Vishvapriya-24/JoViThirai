1aa6019981b5f7d46196428dbcb3b34c0a99a9cd
Vishvapriya-24 <mvishvapriyamurugan@gmail.com>
2026-01-24 19:43:05 +0530

    updated movie page

--- backend/authMiddleware.js
+++ backend/authMiddleware.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken')
 
 const verifyToken = (req,res,next)=>{
     const token = req.cookies.token;
-    console.log(token);
     if(!token){
         return res.status(401).json({error:"Access Denied No token Provided"});
     }
--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://jovithirai.vercel.app/"],
+  origin: ["http://localhost:5173", "https://jovithirai.vercel.app"],
   credentials: true
 }));
 app.use(express.json());
